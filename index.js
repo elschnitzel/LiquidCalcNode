47aced70e298af0f4ec21f7df79a6cf4d2fdd194
NDO <eater.schnitzel@gmail.com>
2019-08-04 10:42:56 +0200

    created pages

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+// Init App
+const app = express();
+
+//Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//Home Route
+app.get('/', function (req, res) {
+    res.render('index', {
+        title:'Caclulation'
+    });
+});
+
+//Add Route
+app.get('/price', function (req, res) {
+    res.render('price', {
+    });
+});
+
+//Start Server
+app.listen('3000', function () {
+    console.log('Server listening on Port 3000');
+});
